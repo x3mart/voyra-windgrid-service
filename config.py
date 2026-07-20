27bf547d71c9ceb5203e4027466320f01f177bad
x3mart <x3mart@gmail.com>
2026-07-20 15:34:56 +0300

    path fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 HOME = Path.home()
-SOURCE_FOLDER = HOME / "voyra" /"wind-data-service" / "data"
+SOURCE_FOLDER = HOME / "voyra-wind-service" /"wind-data-service" / "data"
 NPZ_FILE_NAME = "windgrid.npz"
 CURRENT_CYCLE_JSON_PATH = SOURCE_FOLDER / "current_cycle.json"
 NPZ_PATH = SOURCE_FOLDER / NPZ_FILE_NAME
